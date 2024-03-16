d3e95f5923ae80c3030e7a6f3f3249dfabd01473
Nicolai Romedahl Henriksen <nih@milestone.dk>
2024-03-16 23:51:00 +0100

    Cleanup

--- BuildNumber/Function1.cs
+++ BuildNumber/Function1.cs
@@ -6,27 +6,20 @@ using Microsoft.Extensions.Logging;
 
 namespace BuildNumber
 {
-    public class Function1
+    public class Function1(ILogger<Function1> logger)
     {
-        private readonly ILogger<Function1> _logger;
-
-        public Function1(ILogger<Function1> logger)
-        {
-            _logger = logger;
-        }
-
         [Function("BuildNumber")]
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "next/{id}")] HttpRequest req, string id)
         {
-            _logger.LogInformation("Next build number requested for id: '{id}'", id);
+            logger.LogInformation("Next build number requested for id: '{id}'", id);
 
             try
             {
                 string connectionString = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING")!;
-                _logger.LogInformation("Connection string read: {connStringrRead}", !string.IsNullOrEmpty(connectionString));
+                logger.LogInformation("Connection string read: {connStringrRead}", !string.IsNullOrEmpty(connectionString));
                 using SqlConnection connection = new(connectionString);
                 connection.Open();
-                _logger.LogInformation("DB connection opened");
+                logger.LogInformation("DB connection opened");
                 using SqlCommand readCommand = new("SELECT BuildNumber FROM [dbo].[build_numbers] WITH(ROWLOCK) WHERE BuildIdentifier = @id", connection);
                 readCommand.Parameters.AddWithValue("@id", id);
                 int nextBuildNumber = (int) readCommand.ExecuteScalar() + 1;
@@ -34,17 +27,17 @@ namespace BuildNumber
                 updateCommand.Parameters.AddWithValue("@nextBuildNumber", nextBuildNumber);
                 updateCommand.Parameters.AddWithValue("@id", id);
                 updateCommand.ExecuteNonQuery();
-                _logger.LogInformation("Next build number for id: '{id}' is {nextBuildNumber}", id, nextBuildNumber);
+                logger.LogInformation("Next build number for id: '{id}' is {nextBuildNumber}", id, nextBuildNumber);
                 return new OkObjectResult($$"""
                 {
                     "buildId": "{{id}}",
-                    "buildNumber": {{nextBuildNumber}},
+                    "buildNumber": {{nextBuildNumber}}
                 }
                 """);
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Error while getting next build number for id: '{id}'\n{exception}", id, ex.ToString());
+                logger.LogError(ex, "Error while getting next build number for id: '{id}'\n{exception}", id, ex.ToString());
                 return new StatusCodeResult(500);
             }
         }
