768cc48df7d1df9fc813a0d7ed5555ef0c6c1049
Nicolai Romedahl Henriksen <nih@milestone.dk>
2024-03-16 14:55:40 +0100

    Change magic number

--- BuildNumber/Function1.cs
+++ BuildNumber/Function1.cs
@@ -18,7 +18,7 @@ namespace BuildNumber
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "next/{id}")] HttpRequest req, string id)
         {
             _logger.LogInformation("Next build number requested for id: '{id}'", id);
-            return new OkObjectResult($"Next build for {id} number is: 42");
+            return new OkObjectResult($"Next build for {id} number is: 69");
         }
     }
 }
